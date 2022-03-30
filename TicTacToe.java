8d0e04ac64314f7e4e8223e5b6302b6eed45da0a
Joao Oliveira <thxjao.dev@gmail.com>
2022-03-30 20:18:52 -0300

    feat: added randomize

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class TicTacToe {
     public static void main(String[] args) {
+
+        static ArrayList<Integer> posicoesJogador = new ArrayList<Integer>();
+        static ArrayList<Integer> posicoesComputador = new ArrayList<Integer>();
     
         char[][] tabela =
                          {
@@ -15,42 +22,38 @@ public class TicTacToe {
                 criarTabela(tabela);
 
                 Scanner leitor = new Scanner(System.in);
-                System.out.println("Digite qual casa da tabela você irá escolher (1-9):");
-                int casa = leitor.nextInt();
 
-                System.out.println(casa);
+                while(true) {
+                    System.out.println("Digite qual jogada da tabela você irá escolher (1-9):");
 
-                switch(casa) {
-                    case 1:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 2:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 3:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 4:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 5:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 6:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 7:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 8:
-                        tabela[0][0] = 'X';
-                        break;
-                    case 9:
-                        tabela[0][0] = 'X';
-                        break;                                               
+                    int jogadaJogador = leitor.nextInt();
+                    System.out.println(jogadaJogador);
+                    realizaJogada(tabela, jogadaJogador, "player");
+    
+                    Random randomize = new Random(); 
+                    int jogadaComputador = randomize.nextInt(9) + 1;
+                    realizaJogada(tabela, jogadaComputador, "computador");
+    
+                    criarTabela(tabela);
                 }
-
     }
+
+
+    public static String verificaVencedor() {
+
+            List linhaDeCima = Arrays.asList(1, 2, 3);
+            List linhaDoMeio = Arrays.asList(4, 5, 6);
+            List linhaDeBaixo = Arrays.asList(7, 8, 9);
+
+            List ColunaDoMeio = Arrays.asList(3, 6, 9);
+            List ColunaDaDireita = Arrays.asList(1, 5, 9);
+            List colunaDaEsquerda = Arrays.asList(7, 5, 3);
+
+            return "";
+    }
+
+
+
 
     public static void criarTabela(char[][] tabela) {
         for(char[] linha : tabela) {
@@ -59,6 +62,50 @@ public class TicTacToe {
             }
             System.out.println();
         }
+    }
+
+    public static void realizaJogada(char[][] tabela, int jogada, String user) {
+
+        char chave = 'X';
+
+        if(user.equals("player")) {
+            chave = 'X';
+        } else if(user.equals("computador")) {
+            chave = 'O';
+        }
+
+        switch(jogada) {
+            case 1:
+                tabela[0][0] = chave;
+                break;
+            case 2:
+                tabela[0][2] = chave;
+                break;
+            case 3:
+                tabela[0][4] = chave;
+                break;
+            case 4:
+                tabela[2][0] = chave;
+                break;
+            case 5:
+                tabela[2][2] = chave;
+                break;
+            case 6:
+                tabela[2][4] = chave;
+                break;
+            case 7:
+                tabela[4][0] = chave;
+                break;
+            case 8:
+                tabela[4][2] = chave;
+                break;
+            case 9:
+                tabela[4][4] = chave;
+                break;    
+            default:
+                break;                                           
+        }
+
     }
 
 }
