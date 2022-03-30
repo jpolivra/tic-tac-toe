0c059e8bc31f96b687290aee0f9c64623832879f
Joao Oliveira <thxjao.dev@gmail.com>
2022-03-30 18:56:47 -0300

    feat: added switch case and isolate builder function

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
     
@@ -11,6 +13,43 @@ public class TicTacToe {
                         };
 
                 criarTabela(tabela);
+
+                Scanner leitor = new Scanner(System.in);
+                System.out.println("Digite qual casa da tabela você irá escolher (1-9):");
+                int casa = leitor.nextInt();
+
+                System.out.println(casa);
+
+                switch(casa) {
+                    case 1:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 2:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 3:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 4:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 5:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 6:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 7:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 8:
+                        tabela[0][0] = 'X';
+                        break;
+                    case 9:
+                        tabela[0][0] = 'X';
+                        break;                                               
+                }
+
     }
 
     public static void criarTabela(char[][] tabela) {
